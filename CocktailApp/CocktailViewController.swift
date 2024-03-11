c7f8831d318c04e0beab7cd9fc3c60dcae32b959
Даниил Мурзин <danmur92@mail.ru>
2024-03-11 13:27:18 +0500

    init commit

---
+++ CocktailApp/CocktailViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class CocktailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    private func setupUI(){
+        
+    }
+    private func setupConstraints(){
+        
+    }
+
+}
+
