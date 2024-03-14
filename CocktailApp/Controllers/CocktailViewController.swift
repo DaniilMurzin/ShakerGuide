8f10c2a44262ad11120e6c2ab3ad9af317ef6f10
Даниил Мурзин <danmur92@mail.ru>
2024-03-14 18:33:12 +0500

    refactor Network Manager

--- CocktailApp/Controllers/CocktailViewController.swift
+++ CocktailApp/Controllers/CocktailViewController.swift
@@ -49,30 +49,51 @@ class CocktailViewController: UIViewController {
     }
     
     // MARK: - Private methods
+    
+    private func fetchCocktailData(for cocktailName: String) async {
+        
+        guard let urlString = "https://api.api-ninjas.com/v1/cocktail?name=\(cocktailName)".addingPercentEncoding(
+            withAllowedCharacters: .urlQueryAllowed
+        ),
+              let url = URL(string: urlString)
+        else { return }
+        
+        do {
+            let cocktailData = try await NetworkManager.shared.retrieveData(from: url)
+            DispatchQueue.main.async {
+                self.cocktailName.text = cocktailData[0].name
+                self.updateIngredientsLabel(with: cocktailData[0].ingredients)
+                self.instructions.text = cocktailData[0].instructions
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
     private func updateIngredientsLabel(with ingredients: [String]) {
         let ingredientsText = ingredients.joined(separator: "\n")
         DispatchQueue.main.async {self.ingredients.text = ingredientsText}
     }
-    
-    private func fetchData(_ cocktailName: String) {
-        NetworkManager.shared.fetchData(cocktailName: cocktailName) { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case .success(let data):
-                self.dismissLoadingView()
-                DispatchQueue.main.async {
-                    if !data.isEmpty {
-                        self.cocktailName.text = data[0].name
-                        self.updateIngredientsLabel(with: data[0].ingredients)
-                        self.instructions.text = data[0].instructions
-                    }
-                }
-            case .failure(let error):
-                self.dismissLoadingView()
-                print(error.rawValue)
-            }
-        }
-    }
+
+//
+//    private func fetchData(_ cocktailName: String) {
+//        NetworkManager.shared.fetchData(cocktailName: cocktailName) { [weak self] result in
+//            guard let self = self else { return }
+//            switch result {
+//            case .success(let data):
+//                self.dismissLoadingView()
+//                DispatchQueue.main.async {
+//                    if !data.isEmpty {
+//                        self.cocktailName.text = data[0].name
+//                        self.updateIngredientsLabel(with: data[0].ingredients)
+//                        self.instructions.text = data[0].instructions
+//                    }
+//                }
+//            case .failure(let error):
+//                self.dismissLoadingView()
+//                print(error.rawValue)
+//            }
+//        }
+//    }
     
     private func setupUI(){
         view.backgroundColor = .white
@@ -100,7 +121,10 @@ extension CocktailViewController: UISearchResultsUpdating {
                          !text.isEmpty
         else { return }
         
-        fetchData(text)
+        Task {
+            await fetchCocktailData(for: text)
+                }
+//        fetchData(text)
     }
     
     private func setupSearchController() {
--- CocktailApp/Service/NetworkManager.swift
+++ CocktailApp/Service/NetworkManager.swift
@@ -1,54 +1,71 @@
 import Foundation
 
-class NetworkManager {
-    
-    static let shared = NetworkManager()
-    
-    private init() {}
-    
-    func fetchData(cocktailName request: String, completed: @escaping (Result<Cocktail, GFError>) -> Void) {
-        
-        
-        guard let cocktail = request.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-              
-                let url = URL(string: "https://api.api-ninjas.com/v1/cocktail?name=\(cocktail.lowercased())"
-              )
-                
-        else {
-            completed(.failure(GFError.invalidURL))
-            return
+//class NetworkdManager {
+//
+//    static let shared = NetworkdManager()
+//
+//    private init() {}
+//
+//    func fetchData(cocktailName request: String, completed: @escaping (Result<Cocktail, GFError>) -> Void) {
+//
+//
+//        guard let cocktail = request.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+//
+//                let url = URL(string: "https://api.api-ninjas.com/v1/cocktail?name=\(cocktail.lowercased())"
+//              )
+//
+//        else {
+//            completed(.failure(GFError.invalidURL))
+//            return
+//        }
+//
+//        var urlRequest = URLRequest(url: url)
+//        urlRequest.setValue(
+//            "GnHE8tlwugEwcUP75rr4AQ==3eJLRuGwlxJgOo6k",
+//            forHTTPHeaderField: "X-Api-Key"
+//        )
+//
+//        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+//
+//            if let _ = error {
+//                completed(.failure(.unableToComplete))
+//            }
+//
+//            guard let response = response  as? HTTPURLResponse,
+//                  response.statusCode == 200 else {
+//                  completed(.failure(.invalidResponse))
+//                return
+//            }
+//            guard let data = data else {
+//                completed(.failure(.invalidData))
+//                return
+//            }
+//
+//            do {
+//                let decoder = JSONDecoder()
+//                let quote = try decoder.decode(Cocktail.self, from: data)
+//                completed(.success(quote))
+//            } catch {
+//                completed(.failure(GFError.invalidData))
+//            }
+//        }
+//        task.resume()
+//    }
+//
+    final class NetworkManager {
+
+        static let shared   = NetworkManager()
+
+        private init() {}
+
+        func retrieveData(from url: URL) async throws -> Cocktail {
+            let session = URLSession.shared
+            var request = URLRequest(url: url)
+            request.addValue("GnHE8tlwugEwcUP75rr4AQ==3eJLRuGwlxJgOo6k", forHTTPHeaderField: "X-Api-Key")
+            let (data, _) = try await session.data(for: request)
+            let decoder = JSONDecoder()
+            print(String(data: data, encoding: .utf8) ?? "No data")
+            return try decoder.decode(Cocktail.self, from: data)
         }
-        
-        var urlRequest = URLRequest(url: url)
-        urlRequest.setValue(
-            "GnHE8tlwugEwcUP75rr4AQ==3eJLRuGwlxJgOo6k",
-            forHTTPHeaderField: "X-Api-Key"
-        )
-        
-        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-            
-            if let _ = error {
-                completed(.failure(.unableToComplete))
-            }
-            
-            guard let response = response  as? HTTPURLResponse,
-                  response.statusCode == 200 else {
-                  completed(.failure(.invalidResponse))
-                return
-            }
-            guard let data = data else {
-                completed(.failure(.invalidData))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let quote = try decoder.decode(Cocktail.self, from: data)
-                completed(.success(quote))
-            } catch {
-                completed(.failure(GFError.invalidData))
-            }
-        }
-        task.resume()
     }
-}
+
